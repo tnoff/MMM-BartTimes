2df4c80808244a050d1eda33667d041ba5206cac
tnoff <1326564+tnoff@users.noreply.github.com>
2023-03-13 23:19:21 -0700

    Add service advisories, switch to needle for requests

--- MMM-BartTimes.js
+++ MMM-BartTimes.js
@@ -14,9 +14,13 @@ Module.register("MMM-BartTimes", {
         var self = this;
 
         this.getDepartureInfo()
+        this.getAdvisoryInfo()
         // Schedule update timer.
         setInterval(function() {
             self.getDepartureInfo()
+        }, this.config.updateInterval);
+        setInterval(function() {
+            self.getAdvisoryInfo()
         }, this.config.updateInterval);
     },
 
@@ -32,24 +36,33 @@ Module.register("MMM-BartTimes", {
             config: this.config
         });
     },
+    getAdvisoryInfo: function() {
+        Log.info("Requesting advisory info");
+
+        this.sendSocketNotification("GET_SERVICE_ADVISORY", {
+            config: this.config
+        });
+    },
 
     // Override dom generator.
     getDom: function() {
         var wrapper = document.createElement("div");
 
-        if (!this.info) {
+        if (!this.train_info) {
             wrapper.innerHTML = "LOADING";
             wrapper.className = "dimmed light small";
             return wrapper;
         }
 
+        var wrapper = document.createElement("div");
         var table = document.createElement("table");
         table.className = "small";
+        wrapper.appendChild(table);
 
-        this.info.trains.forEach(train_name => {
+        this.train_info.trains.forEach(train_name => {
 
             if (this.config.train_blacklist.includes(train_name)) {
-                console.log('gottem')
+                console.log('Ignoring train name in blacklist:' + train_name)
                 return;
             }
 
@@ -61,7 +74,7 @@ Module.register("MMM-BartTimes", {
             trainCell.innerHTML = train_name;
             row.appendChild(trainCell);
 
-            this.info[train_name].forEach( time_to_departure => {
+            this.train_info[train_name].forEach( time_to_departure => {
                 var timeCell = document.createElement("td");
                 timeCell.className = "time";
                 if (!isNaN(time_to_departure)) {
@@ -71,15 +84,38 @@ Module.register("MMM-BartTimes", {
                 row.appendChild(timeCell);
             });
         });
+        this.advisory_info.forEach(advisory => {
+            var lower = 0;
+            var higher = 75;
+            if(higher > advisory.length){
+                higher = advisory.length - 1;
+            }
+            var substring = advisory.substring(lower, higher);
+            while(substring != ''){
+                var row = document.createElement("p");
+                wrapper.appendChild(row);
+                row.innerHTML = substring;
+                row.style = "background-color:Tomato;";
+                lower = lower + 75;
+                if(lower > advisory.length){
+                    break;
+                }
+                higher = higher + 75;
+                if(higher > advisory.length){
+                    higher = advisory.length - 1;
+                }
+                substring = advisory.substring(lower, higher);
+            }
+        });
 
-        return table;
+        return wrapper;
     },
 
     // Override get header function
     getHeader: function() {
-        if (this.info) {
-            console.log(this.info.station_name);
-            return this.info.station_name + ' BART Departure Times';
+        if (this.train_info) {
+            console.log(this.train_info.station_name);
+            return this.train_info.station_name + ' BART Departure Times';
         }
         return 'BART Departure Times';
     },
@@ -87,7 +123,11 @@ Module.register("MMM-BartTimes", {
     // Override notification handler.
     socketNotificationReceived: function(notification, payload) {
         if (notification === "DEPARTURE_TIMES") {
-            this.info = payload
+            this.train_info = payload
+            this.updateDom();
+        }
+        if (notification === "SERVICE_ADVISORY") {
+            this.advisory_info = payload
             this.updateDom();
         }
     },
--- node_helper.js
+++ node_helper.js
@@ -1,8 +1,9 @@
-var request = require('request');
+var needle = require('needle');
 var NodeHelper = require("node_helper");
 var { URL } = require('url');
 
-BASE_URL = 'https://api.bart.gov/api/etd.aspx';
+TRAIN_BASE_URL = 'https://api.bart.gov/api/etd.aspx';
+ADVISORY_BASE_URL = 'https://api.bart.gov/api/bsa.aspx';
 
 module.exports = NodeHelper.create({
 
@@ -13,12 +14,20 @@ module.exports = NodeHelper.create({
     // Create a url to get estimated times of depature from the given station
     // using the API key
     build_search_url: function(station, key) {
-        search_url = new URL(BASE_URL);
+        search_url = new URL(TRAIN_BASE_URL);
         search_url.searchParams.set('cmd', 'etd');
         search_url.searchParams.set('json', 'y');
         search_url.searchParams.set('key', key);
         search_url.searchParams.set('orig', station);
         return search_url
+    },
+    build_advisory_url: function(key) {
+        advisory_url = new URL(ADVISORY_BASE_URL);
+        advisory_url.searchParams.set('cmd', 'bsa');
+        advisory_url.searchParams.set('json', 'y');
+        advisory_url.searchParams.set('key', key);
+        advisory_url.searchParams.set('date', 'today');
+        return advisory_url
     },
 
     socketNotificationReceived: function(notification, payload) {
@@ -28,13 +37,11 @@ module.exports = NodeHelper.create({
         if(notification === "GET_DEPARTURE_TIMES") {
 
             var bart_url = this.build_search_url(payload.config.station, payload.config.key);
-
-            request(bart_url.href, function (error, response, body) {
+            needle.get(bart_url.href, function (error, response) {
                 var departure_times = {};
                 departure_times.trains = [];
                 if (!error && response.statusCode == 200) {
-
-                    trains = JSON.parse(body).root.station[0];
+                    trains = response.body.root.station[0];
                     departure_times.station_name = trains.name;
 
                     trains.etd.forEach(train => {
@@ -48,7 +55,29 @@ module.exports = NodeHelper.create({
                     self.sendSocketNotification("DEPARTURE_TIMES", departure_times);
                 }
                 else {
-                    console.log("Bart Loading failed", error, response.statusCode);
+                    console.log("Bart Loading failed", error, response.statusCode, response.body);
+                }
+            });
+        }
+        if(notification === "GET_SERVICE_ADVISORY") {
+
+            var bart_url = this.build_advisory_url(payload.config.key);
+            needle.get(bart_url.href, function (error, response) {
+                var service_advisories = [];
+                if (!error && response.statusCode == 200) {
+                    advisories = response.body.root.bsa
+                    console.log("advisories", advisories);
+                    advisories.forEach(advisory => {
+                        if(advisory.station != '' && (advisory.station.toLowerCase() == payload.config.station.toLowerCase() || advisory.station.toLowerCase() == 'bart')) {
+                            service_advisories.push(advisory.description['#cdata-section'])
+                        }
+                    });
+
+                    console.log("Service Advisories:" + service_advisories);
+                    self.sendSocketNotification("SERVICE_ADVISORY", service_advisories);
+                }
+                else {
+                    console.log("Bart Loading failed", error, response.statusCode, response.body);
                 }
             });
         }
